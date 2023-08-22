f6384c52a1cc5d5cffbcad6573fadec1eaef767a
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2023-08-22 15:49:12 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'XXXX User Documentation'
+copyright = ' '
+author = ' '
 
 release = '0.1'
 version = '0.1.0'
