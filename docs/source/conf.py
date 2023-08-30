681a87a7716e59f08e56acf25f8c68859360968f
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2023-08-30 12:06:28 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,6 @@ html_static_path = ['_static']
 # html_logo = "_static/BlockI-NCSA-Full-Color-RGB_2.png"
 html_logo = "_static/SUPER_FullColor_RGB.png"
 html_theme_options = {
-    'logo_only': False,
+    'logo_only': True,
     'display_version': False,
 }
