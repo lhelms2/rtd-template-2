fec72a73cf494140e375856a35107858dcc1ddad
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2023-09-01 13:32:59 -0500

    Update custom.js

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
   project_name_link.setAttribute("href", "https://lhelms2-rtd-template-2.readthedocs.io/en/logo-test/#");
   project_name_link.classList.add("icon");
   project_name_link.classList.add("icon-home");
-  wysidenavsearch = document.querySelector("body > div > nav > div > div.wy-side-nav-search > a");
+  wysidenavsearch = document.querySelector("body > div > nav > div > div.wy-side-nav-search > a > img");
   wysidenavsearch.insertBefore(project_name_link, wysidenavsearch.a);
 
 });
