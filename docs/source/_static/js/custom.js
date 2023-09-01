89b87c1b342a6c8574b6f276e712714b4549dac7
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2023-09-01 11:46:13 -0500

    Update custom.js

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -8,6 +8,6 @@ $( document ).ready(function() {
   project_name_link.classList.add("icon");
   project_name_link.classList.add("icon-home");
   wysidenavsearch = document.querySelector("body > div > nav > div > div.wy-side-nav-search > a > a");
-  wysidenavsearch.insertBefore(project_name_link, null);
+  wysidenavsearch.insertBefore(project_name_link, a);
 
 });
