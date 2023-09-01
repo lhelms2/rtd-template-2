feee4e0c23135e6ade5145959a9a99f07fa14647
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2023-09-01 10:48:39 -0500

    Update custom.js

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
   // Insert Project Name "XXXX User Documentation" below html_logo in sidebar navigation
   var project_name_link = document.createElement("a");
   var project_name_text = document.createTextNode(" XXXX User Documentation");
-  project_name_link.appendChild(project_name_text);
+  project_name_link.insertBefore(project_name_text);
   project_name_link.setAttribute("href", "https://lhelms2-rtd-template-2.readthedocs.io/en/logo-test/#");
   project_name_link.classList.add("icon");
   project_name_link.classList.add("icon-home");
