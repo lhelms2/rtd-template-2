70d0cd2a097036a55581a3e26556ce0c4917f45a
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2023-09-01 09:49:47 -0500

    Create custom.js

---
+++ docs/source/_static/js/custom.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+
+  // Insert Project Name "XXXX User Documentation" below html_logo in sidebar navigation
+  var project_name_link = document.createElement("a");
+  var project_name_text = document.createTextNode(" XXXX User Documentation");
+  project_name_link.appendChild(project_name_text);
+  project_name_link.setAttribute("href", "https://lhelms2-rtd-template-2.readthedocs.io/en/logo-test/#");
+  project_name_link.classList.add("icon");
+  project_name_link.classList.add("icon-home");
+  wysidenavsearch = document.querySelector("body > div > nav > div > div.wy-side-nav-search > a");
+  wysidenavsearch.appendChild(project_name_link);
+
+});
