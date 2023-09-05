f40ea711a57b1405b1ff1018b872ad6c7ba1801f
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2023-09-05 10:17:49 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ html_js_files = [
 # -- Logo 
 html_static_path = ['_static']
 # html_logo = "images/BlockI-NCSA-Full-Color-RGB.png"
-html_logo = "images/BlockI-NCSA-Full-Color-RGB_border6.png"
+html_logo = "images/BlockI-NCSA-Full-Color-RGB_border5.png"
 # html_logo = "images/SUPER_FullColor_RGB.png"
 html_theme_options = {
      'logo_only': True,
