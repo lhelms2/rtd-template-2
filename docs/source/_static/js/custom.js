50c24c561fedd2ea88447fb3a04624b1713fbbc9
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2023-09-05 08:17:53 -0500

    Update custom.js

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -1,12 +1,12 @@
 $( document ).ready(function() {
 
-  // Insert Project Name "XXXX User Documentation" below html_logo in sidebar navigation
-  var project_name_link = document.createElement("a");
-  var project_name_text = document.createTextNode(" XXXX User Documentation");
-  project_name_link.appendChild(project_name_text);
-  project_name_link.setAttribute("href", "https://lhelms2-rtd-template-2.readthedocs.io/en/logo-test/#");
-  project_name_link.classList.add("icon");
-  project_name_link.classList.add("icon-home");
+  // Add project name "XXXX User Documentation" below NCSA logo in menu pane
+  var project_link = document.createElement("a");
+  var project_text = document.createTextNode(" XXXX User Documentation");
+  project_link.appendChild(project_text);
+  project_link.setAttribute("href", "https://lhelms2-rtd-template-2.readthedocs.io/en/logo-test/#");
+  project_link.classList.add("icon");
+  project_link.classList.add("icon-home");
   wysidenavsearch = document.querySelector("body > div > nav > div > div.wy-side-nav-search > a");
-  wysidenavsearch.appendChild(project_name_link);
+  wysidenavsearch.appendChild(project_link);
 });
