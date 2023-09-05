6376dd50589fe68d8f5f43d74357583f685523cb
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2023-09-05 09:08:42 -0500

    Update custom.js

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
   var link = "https://lhelms2-rtd-template-2.readthedocs.io/en/logo-test/#";
   var element = document.createElement("a");
   element.setAttribute("href", link);
+  element.setAttribute("class", "icon icon-home");
   element.innerHTML = " XXXXX User Documentation"
   wysidenavsearch = document.querySelector("body > div > nav > div > div.wy-side-nav-search > a");
   wysidenavsearch.appendChild(element);
