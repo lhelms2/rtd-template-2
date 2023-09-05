f28f832b03b30be2e256897b7f9ef8f519ae9fbb
Lillian Helms <141255627+lhelms2@users.noreply.github.com>
2023-09-05 09:00:24 -0500

    Update custom.js

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -1,17 +1,3 @@
-// $( document ).ready(function() {
-
-  // Add project name "XXXX User Documentation" below NCSA logo in menu pane
-//  var project_link = document.createElement("a");
-//  var project_text = document.createTextNode(" XXXX User Documentation");
-//  project_link.appendChild(project_text);
-//  project_link.setAttribute("href", "https://lhelms2-rtd-template-2.readthedocs.io/en/logo-test/#");
-//  project_link.classList.add("icon");
-//  project_link.classList.add("icon-home");
-//  wysidenavsearch = document.querySelector("body > div > nav > div > div.wy-side-nav-search > a");
-//  wysidenavsearch.appendChild(project_link);
-// });
-
-
 $( document ).ready(function() {
 
   var link = "https://lhelms2-rtd-template-2.readthedocs.io/en/logo-test/#";
